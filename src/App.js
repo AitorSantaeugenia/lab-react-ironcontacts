9bc43ccefee563ab74932cbc96987d3d7960dd52
AitorSantaeugenia <aitor.j.s.m@gmail.com>
2021-09-21 10:00:39 +0200

    iteration 1 completed

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
+// src/App.js
 import './App.css';
+// Importing contacts
+import contacts from './contacts.json';
+//importing useState
+import { useState } from 'react';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	//contacts 5 max
+	const contactsFiveMax = contacts.splice(0, 5);
+	const [ actors ] = useState(contactsFiveMax);
+
+	//console.log(contactsFiveMax);
+	return (
+		<div className="App">
+			<div className="tableDiv">
+				<h1>IronContacts</h1>
+
+				{actors.map((actors) => {
+					return (
+						<table className="tableActors">
+							<tr>
+								<th> Picture </th>
+								<th> Name </th>
+								<th> Popularity </th>
+							</tr>
+							<tr>
+								<td>
+									{' '}
+									<img src={actors.pictureUrl} width="80px" height="100px" alt="actorphoto" />{' '}
+								</td>
+								<td> {actors.name} </td>
+								{/* Number.toFixed para redondear */}
+								<td> {Number(actors.popularity).toFixed(2)}</td>
+							</tr>
+						</table>
+					);
+				})}
+			</div>
+		</div>
+	);
 }
 
 export default App;
